8017478c8226dcb27057defe5b4825e906c6d705
Omnia-Goher <omnia.goher9@gmail.com>
2023-03-12 19:34:22 +0200

    lab5

---
+++ NodeJS/lab5/Models/courseModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+var DB_URL = "mongodb://localhost:27017/class";
+
+mongoose.connect(DB_URL, { useNewUrlParser:true });
+
+let courseSchema = new mongoose.Schema({
+        courseName:{type: String, pattern:"^[a-zA-Z]+$",required:true},
+        grade:{type: String, enum:["Good","Very Good","Excellent"],required:true}
+});
+
+module.exports = mongoose.model("courses", courseSchema);
