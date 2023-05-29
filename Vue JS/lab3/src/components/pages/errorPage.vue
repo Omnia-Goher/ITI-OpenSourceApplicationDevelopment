1db7546f1bc512156b4649b58ea284f06769b5a3
Omnia-Goher <omnia.goher9@gmail.com>
2023-05-29 22:46:35 +0300

    lab3

---
+++ Vue JS/lab3/src/components/pages/errorPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container container-img">
+        <img src="../../assets/images/error.jpg"/>
+    </div>
+</template>
+
+<script>
+export default {
+name: "ErrorPage",
+};
+</script>
+
+<style scoped>
+.container-img {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.container-img img {
+    width: 55%;
+    height: 700px;
+    margin: 5px auto 5px auto;
+}
+</style>
